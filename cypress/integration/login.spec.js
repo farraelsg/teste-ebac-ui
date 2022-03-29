a9a2ebf722f74b200ac52da7616b13fa1a6fb73a
farraelsg <rafaelga7@gmail.com>
2022-03-28 23:16:49 -0300

    meu primeiro commit

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -20,12 +20,22 @@ context('Funcionalidade Login', () =>{
         cy.get('.woocommerce-error').should('contain', 'Endereço de e-mail desconhecido.')
     })
 
-    it.only('Deve exibir uma mensagem de erro ao inserir senha inválida', ()=>{
+    it('Deve exibir uma mensagem de erro ao inserir senha inválida', ()=>{
         cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
         cy.get('#username').type('aluno_ebac@teste.com')
         cy.get('#password').type('teste@teste')
         cy.get('.woocommerce-form > .button').click()
 
         cy.get('.woocommerce-error').should('contain', 'Erro: A senha fornecida para o e-mail')
+    })
+
+    it.only('Deve fazer login com sucesso', () =>{
+        cy.visit('https://lms.ebaconline.com.br/')
+        cy.get('[data-test="auth-form-email-input"] > .base-input').type('farraelega7@gmail.com')
+        cy.get('[data-test="auth-form-password-input"] > .base-input').type('rafaeu159753')
+        cy.get('[data-test="auth-form-login-button"]').click()
+
+        cy.get('.courses-grid__h').should('contain', 'Meus cursos')
+        
     })
 })
